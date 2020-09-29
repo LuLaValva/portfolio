68554cef7669698b4de4f8f0d044405673433e3e
Luke LaValva <lulavalva@gmail.com>
2020-09-29 11:15:37 -0400

    Updates

---
+++ precache-manifest.c8736cfcb89954eb8b0c59f85db847cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c96747728baa1066fbb0da3d42ea99f9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "51ae6b4172511aba5353",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a2dc571c230c411174ae",
+    "url": "/portfolio/static/js/2.e083310c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.e083310c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51ae6b4172511aba5353",
+    "url": "/portfolio/static/js/main.b7263222.chunk.js"
+  },
+  {
+    "revision": "cbc5c7d2f0629954e339",
+    "url": "/portfolio/static/js/runtime-main.2fc27b3b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
