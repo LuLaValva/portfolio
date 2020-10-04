bb1eb30f15dfc23ac585dd6e0b28c35366bc32eb
Luke LaValva <lulavalva@gmail.com>
2020-10-03 22:06:31 -0400

    Set up central site

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import Header from "./Elements/Header/Header";
+import Body from "./Elements/Body/Body";
+import Footer from "./Elements/Footer/Footer";
+import "./App.css";
 
 function App() {
-  return <Header />;
+  return (
+    <div id="fullPage">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Elements/Body/Body.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Body.css";
+
+function Body() {
+  return <div id="body">Here is some body text</div>;
+}
+
+export default Body;
