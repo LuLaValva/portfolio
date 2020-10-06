caa81374506d324eedc2a73b4289e9103707223f
Luke LaValva <lulavalva@gmail.com>
2020-10-06 07:43:51 -0400

    Added Social Media

--- src/Elements/Footer/Footer.js
+++ src/Elements/Footer/Footer.js
@@ -2,7 +2,22 @@ import React from "react";
 import "./Footer.css";
 
 function Footer() {
-  return <div id="footer">Here is some Footer text</div>;
+  return (
+    <div id="footer">
+      <a href="https://github.com/LuLaValva">
+        <span className="fa fa-github"></span>
+      </a>
+      <a href="https://www.linkedin.com/in/luke-lavalva/">
+        <span className="fa fa-linkedin"></span>
+      </a>
+      <a href="https://www.youtube.com/channel/UC3YKU-61ekP7PajiFH8GvrA?view_as=subscriber">
+        <span className="fa fa-youtube-play"></span>
+      </a>
+      <a href="https://stackoverflow.com/users/9785375/lucas-lavalva">
+        <span className="fa fa-stack-overflow"></span>
+      </a>
+    </div>
+  );
 }
 
 export default Footer;
