860eadf302944710a90161eedbbf034a8c48cf55
Luke LaValva <lulavalva@gmail.com>
2020-10-14 21:53:46 -0400

    Finished the confidence table object

--- src/Elements/Body/Body.js
+++ src/Elements/Body/Body.js
@@ -2,17 +2,32 @@ import React from "react";
 import "./Body.css";
 import BodyElement from "./BodyElement";
 import ProgrammingLanguages from "../../FrequentlyUpdatedInfo/programmingLanguages";
-import ConfidenceTable from "./ConfidenceTable";
+import FamiliarSoftware from "../../FrequentlyUpdatedInfo/familiarSoftware";
+import ConfidenceTable from "./ConfidenceTable/ConfidenceTable";
 
 function Body() {
   return (
     <div id="body">
-      <BodyElement title="Education">Testing Body Element</BodyElement>
+      <BodyElement title="About Me">
+        <p>
+          Hello! My name is Luke LaValva. I'm currently a full time student at
+          Rowan University with a double major in Computer Science and
+          Mathematics, and a minor in Sociology. I haven't entirely decided on
+          something to dedicate my life to yet, mostly because it seems as
+          though I'm passionate about <i>everything</i>. The most recent
+          additions to my assortment of interests are 3d modeling software, most
+          particularly Blender.
+        </p>
+      </BodyElement>
       <BodyElement title="Programming Languages">
         <ConfidenceTable info={ProgrammingLanguages} />
       </BodyElement>
-      <BodyElement title="Employment History">Employment</BodyElement>
+      <BodyElement title="Software">
+        <ConfidenceTable info={FamiliarSoftware} />
+      </BodyElement>
+      <BodyElement title="Employment">Employment</BodyElement>
       <BodyElement title="Related Experience">Experience</BodyElement>
+      <BodyElement title="Education">Education</BodyElement>
       <BodyElement title="Honors, Awards, Acheivements">
         Some Acheivements
       </BodyElement>
---
+++ src/FrequentlyUpdatedInfo/familiarSoftware.js
@@ -0,0 +1,29 @@
+export default {
+  Blender: {
+    confidence: 90,
+    info:
+      "I spent the summer of 2020 ABSORBED in Blender, and learned everything I possibly could about it",
+    projects: [],
+  },
+  "Google Suite": {
+    confidence: 100,
+    info:
+      "I'm very familiar with Google Docs/Slides, and I frequently use Google Apps Scipt to enhance my Google Sheets experience",
+    projects: [],
+  },
+  "Microsoft Office": {
+    confidence: 70,
+    info: "In my youth I used to make animations using PowerPoint",
+    projects: [],
+  },
+  iMovie: {
+    confidence: 80,
+    info: "I made many ridiculous videos with my brother using iMovie",
+    projects: [],
+  },
+  "AutoDesk Inventor": {
+    confidence: 50,
+    info: "I took a class in high school which required autoCAD proficiency",
+    projects: [],
+  },
+};
